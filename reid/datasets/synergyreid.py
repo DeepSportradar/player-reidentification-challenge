c2a86caf8b4155748b4d8208bf45f07f94a42992
Davide Zambrano <davide.zambrano@gmail.com>
2023-06-15 22:45:16 +0200

    updating file

--- reid/datasets/synergyreid.py
+++ reid/datasets/synergyreid.py
@@ -11,7 +11,7 @@ from ..utils.serialization import read_json, write_json
 
 
 class SynergyReID(Dataset):
-    md5 = "98fe8b85c154923742b7c14418f2b2d7"
+    md5 = "f26a0457ffd0169e753168a03103ba8f"
 
     def __init__(self, root, split_id=0, num_val=100, download=True):
         super(SynergyReID, self).__init__(root, split_id=split_id)
